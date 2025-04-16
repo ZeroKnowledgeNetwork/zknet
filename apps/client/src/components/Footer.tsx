3a72e9446deaa5139c6a06d5954cde81014f2fd0
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2025-04-16 16:30:59 -0700

    feat(client/updater): add managed Updater component to Footer

--- apps/client/src/components/Footer.tsx
+++ apps/client/src/components/Footer.tsx
@@ -1,20 +1,53 @@
+import { useEffect, useState } from "react";
 import { useStore } from "../store";
-import { IconCommandLine } from ".";
+import { IconArrowPath, IconCheckBadge, IconCommandLine } from ".";
+import { Updater } from "./Updater";
 
 export function Footer() {
   const appVersion = useStore((s) => s.appVersion);
-  const platformArch = useStore((s) => s.platformArch);
   const consoleLines = useStore((s) => s.consoleLines);
+  const platformArch = useStore((s) => s.platformArch);
+  const updateStatus = useStore((s) => s.updateStatus);
+
+  const [updaterKey, setUpdaterKey] = useState(0);
+  const [updaterVisibility, setUpdaterVisibility] = useState(true);
+
+  useEffect(() => {
+    if (updateStatus === "checked-current") {
+      const timer = setTimeout(() => setUpdaterVisibility(false), 3000);
+      return () => clearTimeout(timer); // Cleanup on unmount
+    }
+  }, [updateStatus]);
+
+  const isUpdaterVisible =
+    updaterVisibility ||
+    updateStatus === "checked-updatable" ||
+    updateStatus === "installed";
+
+  const showUpdater = () =>
+    updateStatus === "error"
+      ? setUpdaterKey((p) => p + 1) // remount to re-try updates
+      : setUpdaterVisibility(true); // show but don't toggle
 
   return (
     <div>
+      {isUpdaterVisible && <Updater key={updaterKey} />}
       <div className="bg-base-200 text-base-content/30 border-t border-base-300 rounded-none flex w-full items-center py-2">
-        <div className="mx-auto flex gap-x-4 text-sm">
+        <div className="mx-auto flex gap-x-4 text-sm items-center">
           <span>ZKNetwork Client</span>
           <span>|</span>
-          <span>Version: {appVersion}</span>
-          <span>|</span>
           <span>Platform: {platformArch}</span>
+          <span>|</span>
+          <span onClick={showUpdater} className="flex items-center">
+            Version: {appVersion}
+            <button className="btn btn-ghost btn-xs btn-primary p-1">
+              {updateStatus === "checked-current" ? (
+                <IconCheckBadge className="size-5" />
+              ) : (
+                <IconArrowPath className="size-5" />
+              )}
+            </button>
+          </span>
         </div>
       </div>
       <div className="collapse collapse-arrow bg-base-200 text-base-content/30 border-t border-base-300 rounded-none">

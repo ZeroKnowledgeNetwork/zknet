6b8a81cbeec9ff1ea276ea52b0350ed275d0d8d0
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2025-06-16 17:06:46 -0700

    feat(ext): add ZKNet Client API status handling in background

--- apps/ext/entrypoints/background.ts
+++ apps/ext/entrypoints/background.ts
@@ -1,7 +1,50 @@
+import config from '@/src/config';
 import { packFetchResponse } from '@/src/fetch-wire';
 import { extensionMsgr } from '@/src/messaging/extension';
+import { JsonRpcWebSocket } from '@/src/ws-rpc-client';
 
+type ZKNetClientStatus = {
+  app: {
+    version: string;
+  };
+  network: {
+    isConnected: boolean;
+  };
+  settings: {
+    walletshield: {
+      listenAddress: string;
+    };
+  };
+};
 export default defineBackground(() => {
+  let clientStatus: ZKNetClientStatus | null = null;
+
+  const url = `http://${config.ZKNET_CLIENT_HOST}:${config.ZKNET_CLIENT_PORT}`;
+  const rpc = new JsonRpcWebSocket(url, {
+    onNotification: (method, params) => {
+      switch (method) {
+        case 'status':
+          clientStatus = params;
+          break;
+
+        default:
+          break;
+      }
+    },
+
+    onOpen: async (ws) => {
+      try {
+        clientStatus = await ws.call('getStatus');
+      } catch (err) {
+        // ignore errors here
+      }
+    },
+
+    onClose: () => {
+      clientStatus = null;
+    },
+  });
+
   extensionMsgr.onMessage('zknet.fetch', async ({ data: { input, init } }) => {
     try {
       const res = await fetch(input, init);

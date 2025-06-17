f50dc2887f4d2e5e4c68aa3a46ae1f27a927cfbf
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2025-06-16 17:43:02 -0700

    feat(ext): fetch using port set by client settings

--- apps/ext/entrypoints/background.ts
+++ apps/ext/entrypoints/background.ts
@@ -16,6 +16,10 @@ type ZKNetClientStatus = {
     };
   };
 };
+
+const extractPort = (s: string): string | null =>
+  s.match(/:(\d{1,5})(?=$|[/?#])/)?.[1] ?? null;
+
 export default defineBackground(() => {
   let clientStatus: ZKNetClientStatus | null = null;
 
@@ -47,7 +51,20 @@ export default defineBackground(() => {
 
   extensionMsgr.onMessage('zknet.fetch', async ({ data: { input, init } }) => {
     try {
-      const res = await fetch(input, init);
+      if (!rpc.isConnected() || clientStatus === null)
+        throw new Error('ZKNetwork Client is not running');
+
+      if (!clientStatus.network.isConnected)
+        throw new Error('ZKNetwork is not connected');
+
+      const wla = extractPort(clientStatus.settings.walletshield.listenAddress);
+      if (wla === null) throw new Error('Invalid listen address');
+
+      const url = `http://${config.ZKNET_CLIENT_HOST}:${wla}${
+        input.toString().startsWith('/') ? '' : '/'
+      }${input.toString()}`;
+
+      const res = await fetch(url, init);
       return await packFetchResponse(res);
     } catch (err: any) {
       return await packFetchResponse(err as Error);

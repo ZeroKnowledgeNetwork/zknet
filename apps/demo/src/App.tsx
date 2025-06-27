3e446d417c4caaf57b6accbbff136c446f889602
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2025-06-26 18:26:39 -0700

    feat(demo): integrate ZKNet SDK with fetch example

--- apps/demo/src/App.tsx
+++ apps/demo/src/App.tsx
@@ -1,10 +1,53 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import zknLogo from './assets/zkn.svg';
+import { ZKNetSDK } from '@zknet/sdk';
+import type { ZKNetSDKOptions } from '@zknet/sdk';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const zknetOpts: ZKNetSDKOptions = {};
+
+  const [fetchResult, setFetchResult] = useState<string | null>(null);
+
+  const [hasZKNet, setHasZKNet] = useState<boolean>(false);
+  useEffect(() => {
+    ZKNetSDK.init(zknetOpts)
+      .then(() => setHasZKNet(true))
+      .catch(() => setHasZKNet(false));
+  }, []);
+
+  async function handleFetch() {
+    setFetchResult('');
+
+    try {
+      const zknet = await ZKNetSDK.init(zknetOpts);
+
+      const r = await zknet.fetch('/ethereum', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id: '1729115977487',
+          jsonrpc: '2.0',
+          method: 'eth_chainId',
+          params: [],
+        }),
+      });
+
+      if (!r.ok)
+        throw new Error(`[demo] Fetch failed: ${r.status} ${r.statusText}`);
+
+      const data = await r.json();
+      console.log('[demo] Fetch successful:', data);
+      setFetchResult(JSON.stringify(data));
+    } catch (err) {
+      console.log('[demo] Fetch error:', err);
+      setFetchResult(`${err}`);
+    }
+  }
 
   return (
     <>
@@ -15,24 +58,27 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://zknet.io" target="_blank">
+          <img src={zknLogo} className="logo zkn" alt="ZKN logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card card-border bg-base-200">
+      <h1>Vite + React + ZKNetwork</h1>
+      <h2>Quickstart Template</h2>
+      <div className="card card-border bg-base-200 mt-4">
         <div className="card-body">
-          <button
-            className="btn"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is {count}
+          <button className="btn btn-primary" onClick={handleFetch}>
+            zknet.fetch
           </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
+          <p>{fetchResult}</p>
         </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="card card-border bg-base-200 mt-4">
+        <div className="card-body">
+          {hasZKNet && <span>ZKNet Extension is installed</span>}
+          {hasZKNet || <span>ZKNet Extension is not installed</span>}
+        </div>
+      </div>
     </>
   );
 }

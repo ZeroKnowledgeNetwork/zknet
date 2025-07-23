4e5ff78afbd388dd404bd1a122cb33b30fbbdb2c
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2025-07-22 17:55:31 -0700

    feat(client-cli): add config file and AppContext usage

--- apps/client-cli/src/main.rs
+++ apps/client-cli/src/main.rs
@@ -1,12 +1,14 @@
 use anyhow::Result;
 use clap::Parser;
-use zknet_core::{network_connect, utils::get_platform_arch};
+use zknet_core::{context::AppContext, network_connect, utils::get_platform_arch};
 
-const APP_NAME: &str = "ZKNetwork Client CLI";
+const APP_NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+const CONFIG_JSON: &str = include_str!("../assets/config.json");
+
 #[derive(Parser, Debug)]
-#[command(author, version, about = APP_NAME)]
+#[command(author, version, about = "ZKNetwork Client CLI")]
 struct Cli {
     /// The ID of the network to connect to
     network_id: String,
@@ -16,9 +18,14 @@ struct Cli {
 async fn main() -> Result<()> {
     let cli = Cli::parse();
 
+    let app_name = APP_NAME.replace('_', "-");
     let platform_arch = get_platform_arch().expect("Unsupported platform or architecture");
-    println!("Starting {APP_NAME} v{VERSION} on {platform_arch}");
+    println!("Starting {app_name} v{VERSION} on {platform_arch}");
 
-    network_connect(&cli.network_id).await?;
+    let ctx = AppContext::new(&app_name, CONFIG_JSON);
+    println!("App data directory: {}", ctx.paths.dir_data().display());
+    println!("Using configuration: {:#?}", ctx.config);
+
+    network_connect(ctx, &cli.network_id).await?;
     Ok(())
 }
